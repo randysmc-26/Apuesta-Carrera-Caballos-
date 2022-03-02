596a268835c3f6ab200fbfb894ee2b5e4e467a8b
Randy Sum <randymarcelino-sumcoyoy@cunoc.edu.gt>
2022-03-02 02:51:58 -0600

    Agregaron apuestas y metodo de verificacion

---
+++ src/main/java/com/mycompany/practicaestructura/principal/Main.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.practicaestructura.principal;
+
+import com.mycompany.practicaestructura.actores.ListaCompetidores;
+
+/**
+ *
+ * @author randysmc
+ */
+public class Main {
+    public static void main(String[] args) {
+        ListaCompetidores lc = new ListaCompetidores();
+        
+        lc.agregarParticipante();
+        lc.mostrarApuesta();
+    }
+}
