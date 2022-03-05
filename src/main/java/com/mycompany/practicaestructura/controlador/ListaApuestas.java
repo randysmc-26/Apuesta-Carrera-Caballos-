a1aa3d1e7a959c95e5e7f21835576a652d768103
Randy Sum <randymarcelino-sumcoyoy@cunoc.edu.gt>
2022-03-05 04:23:37 -0600

    correccion carga de archivos y encapsulamiento

--- src/main/java/com/mycompany/practicaestructura/controlador/ListaApuestas.java
+++ src/main/java/com/mycompany/practicaestructura/controlador/ListaApuestas.java
@@ -16,19 +16,25 @@ public class ListaApuestas {
     int contador = 0;
 
     public ListaApuestas() {
-        
+
     }
 
     public void ingresarApostador(String nombre, int monto, int p1, int p2, int p3, int p4, int p5, int p6, int p7, int p8, int p9, int p10) {
         apuestas[contador] = new Apuesta(new Persona(contador, nombre, monto));
         apuestas[contador].getCompetidores().getCompetidores()[0].setPosicionApuesta(p1);
-        apuestas[contador].getCompetidores().getCompetidores()[0].setPosicionApuesta(p2);
-        apuestas[contador].getCompetidores().getCompetidores()[0].setPosicionApuesta(p3);
-        apuestas[contador].getCompetidores().getCompetidores()[0].setPosicionApuesta(p4);
-        
+        apuestas[contador].getCompetidores().getCompetidores()[1].setPosicionApuesta(p2);
+        apuestas[contador].getCompetidores().getCompetidores()[2].setPosicionApuesta(p3);
+        apuestas[contador].getCompetidores().getCompetidores()[3].setPosicionApuesta(p4);
+        apuestas[contador].getCompetidores().getCompetidores()[4].setPosicionApuesta(p5);
+        apuestas[contador].getCompetidores().getCompetidores()[5].setPosicionApuesta(p6);
+        apuestas[contador].getCompetidores().getCompetidores()[6].setPosicionApuesta(p7);
+        apuestas[contador].getCompetidores().getCompetidores()[7].setPosicionApuesta(p8);
+        apuestas[contador].getCompetidores().getCompetidores()[8].setPosicionApuesta(p9);
+        apuestas[contador].getCompetidores().getCompetidores()[9].setPosicionApuesta(p10);
+
+
         //System.out.println(apuestas[contador].getCompetidores());
-        
-       /* apuestas[contador].getCompetidores().competidores[0].setPosicionApuesta(p1);
+        /* apuestas[contador].getCompetidores().competidores[0].setPosicionApuesta(p1);
         apuestas[contador].getCompetidores().competidores[1].setPosicionApuesta(p2);
         apuestas[contador].getCompetidores().competidores[2].setPosicionApuesta(p3);
         apuestas[contador].getCompetidores().competidores[3].setPosicionApuesta(p4);
@@ -38,26 +44,26 @@ public class ListaApuestas {
         apuestas[contador].getCompetidores().competidores[7].setPosicionApuesta(p8);
         apuestas[contador].getCompetidores().competidores[8].setPosicionApuesta(p9);
         apuestas[contador].getCompetidores().competidores[9].setPosicionApuesta(p10);*/
-        
         //System.out.println("El contador antes del incremento es: "+ contador);
         incrementarContador();
         //System.out.println("El contador es: " +contador);
     }
-    
-    public void incrementarContador(){
+
+    public void incrementarContador() {
         contador++;
     }
 
     public void imprimir() {
         for (int i = 0; i < contador; i++) {
-            System.out.println("Mi nombre es: "+ apuestas[i].persona.getNombre() + " aposte: Q" +apuestas[i].persona.getMontoApuesta());
+
+            System.out.println("Mi nombre es: " + apuestas[i].persona.getNombre() + " aposte: Q" + apuestas[i].persona.getMontoApuesta());
             for (int j = 0; j < 10; j++) {
-                            System.out.print("Mi caballo: " + apuestas[i].getCompetidores().getCompetidores()[j].getNombre());
-            System.out.println(" quedara en: "+apuestas[i].getCompetidores().getCompetidores()[j].getPosicionApuesta());
+                System.out.print("Mi caballo: " + apuestas[i].getCompetidores().getCompetidores()[j].getNombre());
+                System.out.println(" quedara en: " + apuestas[i].getCompetidores().getCompetidores()[j].getPosicionApuesta());
             }
-            
+            System.out.println("\n\n");
         }
-            /*System.out.println(apuestas[0].persona.getNombre());
+        /*System.out.println(apuestas[0].persona.getNombre());
             System.out.println(apuestas[0].persona.getMontoApuesta());
             System.out.println(apuestas[0].getCompetidores().competidores[0].getPosicionApuesta());
             System.out.println("Contador: "+contador);
@@ -69,6 +75,5 @@ public class ListaApuestas {
     public int getContador() {
         return contador;
     }
-
 
 }
